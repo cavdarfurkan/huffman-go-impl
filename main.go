c0e6b3ff33f51b7494405e9e6c0bd0b2ca9d9e8c
Furkan Çavdar <m.furkancavdar@gmail.com>
2024-08-14 04:00:35 +0200

    generate-nodes implemented
    frequency table -> minheap(nodes)

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"container/heap"
+
+	"github.com/cavdarfurkan/huffman-go-impl/minheap"
+)
+
 type FrequencyTable map[rune]int
 
 func GenerateFrequencyTable(str string) FrequencyTable {
@@ -9,3 +15,32 @@ func GenerateFrequencyTable(str string) FrequencyTable {
 	}
 	return freqTable
 }
+
+func (freqTable FrequencyTable) GenerateNodes() minheap.MinHeap {
+	mh := make(minheap.MinHeap, 0, len(freqTable))
+	heap.Init(&mh)
+
+	for key, val := range freqTable {
+		heap.Push(&mh, &minheap.Node{Char: key, Frequency: val})
+	}
+
+	return mh
+}
+
+// func main() {
+// 	mh := make(minheap.MinHeap, 0)
+// 	heap.Init(&mh)
+
+// 	heap.Push(&mh, &minheap.Node{Char: 'a', Frequency: 5})
+// 	heap.Push(&mh, &minheap.Node{Char: 'b', Frequency: 1})
+// 	heap.Push(&mh, &minheap.Node{Char: 'c', Frequency: 3})
+
+// 	for mh.Len() > 0 {
+// 		n := heap.Pop(&mh).(*minheap.Node)
+// 		fmt.Print(n)
+// 	}
+// }
+
+// TODO: Generate huffman tree
+// TODO: Encode
+// TODO: Decode
--- main_test.go
+++ main_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"reflect"
 	"testing"
+
+	"github.com/cavdarfurkan/huffman-go-impl/minheap"
 )
 
 func TestFreqTable(t *testing.T) {
@@ -20,3 +22,71 @@ func TestFreqTable(t *testing.T) {
 		}
 	}
 }
+
+func TestGenerateNodes(t *testing.T) {
+	var generateNodesTests = []struct {
+		freqTable FrequencyTable
+		want      minheap.MinHeap
+	}{
+		{
+			GenerateFrequencyTable("deneme"),
+			minheap.MinHeap{
+				&minheap.Node{Char: 'e', Frequency: 3},
+				&minheap.Node{Char: 'd', Frequency: 1},
+				&minheap.Node{Char: 'n', Frequency: 1},
+				&minheap.Node{Char: 'm', Frequency: 1},
+			},
+		},
+		{
+			GenerateFrequencyTable("this is an example of a huffman tree"),
+			minheap.MinHeap{
+				&minheap.Node{Char: ' ', Frequency: 7},
+				&minheap.Node{Char: 'a', Frequency: 4},
+				&minheap.Node{Char: 'e', Frequency: 4},
+				&minheap.Node{Char: 'f', Frequency: 3},
+				&minheap.Node{Char: 'h', Frequency: 2},
+				&minheap.Node{Char: 'i', Frequency: 2},
+				&minheap.Node{Char: 'm', Frequency: 2},
+				&minheap.Node{Char: 'n', Frequency: 2},
+				&minheap.Node{Char: 's', Frequency: 2},
+				&minheap.Node{Char: 't', Frequency: 2},
+				&minheap.Node{Char: 'l', Frequency: 1},
+				&minheap.Node{Char: 'o', Frequency: 1},
+				&minheap.Node{Char: 'p', Frequency: 1},
+				&minheap.Node{Char: 'r', Frequency: 1},
+				&minheap.Node{Char: 'u', Frequency: 1},
+				&minheap.Node{Char: 'x', Frequency: 1},
+			},
+		},
+	}
+
+	for _, test := range generateNodesTests {
+		nodes := test.freqTable.GenerateNodes()
+		nodesMap := make(map[rune]int, nodes.Len())
+
+		// Fill the nodesMap
+		for _, val := range nodes {
+			nodesMap[val.Char] = val.Frequency
+		}
+
+		if nodes.Len() == test.want.Len() {
+			for _, val := range test.want {
+				gotFreq, ok := nodesMap[val.Char]
+				if !ok {
+					t.Errorf("got: %v\t want: %v", nodes, test.want)
+				}
+				if gotFreq != val.Frequency {
+					t.Errorf("Incorrect frequency error\nFor: '%c'\ngot: %d\twant: %d", val.Char, gotFreq, val.Frequency)
+				}
+			}
+		} else {
+			t.Errorf("Length error\ngot: '%d'\twanted: '%d'", nodes.Len(), test.want.Len())
+		}
+	}
+}
+
+// func TestBuildHuffmanTree(t *testing.T) {
+// var huffmanTreeTests = []struct {
+// 	nodes []*minheap.Node
+// }
+// }
